c668bd4437ef882aedd40df2e5f1117cd727dd15
Ralph Njuguna <107020905+ra1phn@users.noreply.github.com>
2022-07-04 22:08:33 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+
+	for (num = '0'; num <= '9'; num++)
+	putchar(num);
+	putchar('\n');
+	return (0);
+}
