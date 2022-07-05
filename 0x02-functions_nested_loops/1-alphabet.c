de1b76f9ea11eb4d18f06c53fd4eb64a4b09745d
Ralph Njuguna <107020905+ra1phn@users.noreply.github.com>
2022-07-05 23:22:23 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - program that prints alphabets2
+ *
+ *
+ *
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
