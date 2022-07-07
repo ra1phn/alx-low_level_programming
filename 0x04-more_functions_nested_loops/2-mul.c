86bde9c52895770c68c4ec8c0dd16c07303bafce
Ralph Njuguna <107020905+ra1phn@users.noreply.github.com>
2022-07-07 23:34:59 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - function to know if
+ * the character is a letter or not
+ *
+ * @c: variable to evaluate
+ *
+ * Return: Get 1 if it's a letter,
+ * 0 otherwise
+ 
+ */
+int mul(int a, int b)
+{
+    int a, b, product;
+    
+    product = a * b;
+ 
+    return product; 
+}
