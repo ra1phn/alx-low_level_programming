e8f156642868692908fc314f0ffc42bfcf714d5a
Ralph Njuguna <107020905+ra1phn@users.noreply.github.com>
2022-07-08 01:14:33 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- * _isupper - function 
+ *_isupper - function 
  *
  * @c: parameter
  *
  * Return: return value
  */
- 
-int _isupper (int c)
+ int _isupper (int c)
 {
-  if (c >= 65 && c <= 90)
-      return(1);
-  else
-      return(0);
+if (c >= 65 && c <= 90)
+return(1);
+else
+return(0);
 }
