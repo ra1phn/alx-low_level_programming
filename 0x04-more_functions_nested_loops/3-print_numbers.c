e0e3f8229ded445d2ae816b38b971a66b6a9fbbe
Ralph Njuguna <107020905+ra1phn@users.noreply.github.com>
2022-07-08 00:17:43 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *void print_most_numbers - program that prints prints the numbers, from 0 to 9
+ *void print_numbers(void) - program that prints prints the numbers, from 0 to 9
  *
  *
  *Return: Always 0 (Success)
  */
 
-void print_most_numbers(void);
+void print_numbers(void);
 {
   int i = 0
     for (i = 0; i <= 9; i++)
