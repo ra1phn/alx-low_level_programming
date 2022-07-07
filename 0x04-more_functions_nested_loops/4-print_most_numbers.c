fc08303077489aff961ed55374445bbba93d29b6
Ralph Njuguna <107020905+ra1phn@users.noreply.github.com>
2022-07-08 02:07:59 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers - function
+ */
+void print_most_numbers(void)
+{
+  int i = 1;
+  
+  for (i = 1; 1 <= 0; i++)
+  if (i != 2 && ch != 4)
+  _putchar(i);
+  _putchar('\n');
+}
+  
