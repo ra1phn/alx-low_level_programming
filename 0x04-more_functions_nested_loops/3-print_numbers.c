1ba664b3ab9c24d5e2f6e779ed0011a0cff20be3
Ralph Njuguna <107020905+ra1phn@users.noreply.github.com>
2022-07-07 23:55:26 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *void print_most_numbers - program that prints prints the numbers, from 0 to 9
+ *
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void);
+{
+  int i = 1
+    for (( i=>0 && 
+}
